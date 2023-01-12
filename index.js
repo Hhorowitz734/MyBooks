31228663b334b26c8640ad5f636762455f5158f5
Benjamin_Horowitz <hhorowitz734@gmail.com>
2023-01-12 16:41:09 -0600

    Created add new book window

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Don't forget to add fonts to stylesheet
+// Controls stars
+const allStars = document.querySelectorAll('.star');
+
+allStars.forEach((star, i) => {
+    star.onclick = function(event){
+        event.preventDefault();
+        allStars.forEach((star, j) => {
+            if (i >= j){
+                star.innerHTML = '&#9733';
+            }
+            else {
+                star.innerHTML = '&#9734;';
+            }
+        });
+    }
+});
+
+//https://www.youtube.com/watch?v=lzK8vM_wdoY <-- USE THIS TO DISPLAY IMAGE LATER
